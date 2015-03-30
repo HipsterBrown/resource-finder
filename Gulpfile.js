4a9115fec6e9303c251e20101f1633b867225bab
Nick Hehr <headhipster@hipsterbrown.com>
2015-03-30 18:25:47 -0400

    adds styles and gulp process for styles, add search urls for bower & npm

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var babelify = require('babelify');
+var sass = require('gulp-sass');
 
 gulp.task('preprocess:js', function(){
   return browserify({
@@ -13,3 +14,16 @@ gulp.task('preprocess:js', function(){
   .pipe(source('app.js'))
   .pipe(gulp.dest('./app/src/browser_action'));
 });
+
+gulp.task('preprocess:css', function(){
+  return gulp.src('./scss/app.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('./app/src/browser_action'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./js/*.js', ['preprocess:js']);
+  gulp.watch('./scss/*.scss', ['preprocess:css']);
+});
+
+gulp.task('default', ['watch']);
--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import NPM from 'npm';
-import Bower from 'bower';
 
 let D = React.DOM;
+let urls = {
+  npm: "http://npm-registry-cors-proxy.herokuapp.com/",
+  bower: "http://bower.herokuapp.com/packages/search/"
+};
 
-
+React.render(D.h1({}, 'Package Search 4 Winners'), document.getElementById('app'));
